1539e3c70bc4999858090dc86c4a7d3311abada8
Adarsh7079 <adarshk8271@gmail.com>
2024-08-06 15:38:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card/Card'
+
+const App = () => {
+  return (
+    <div  className=' bg-gray-800 w-full h-screen'>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/card/Card.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { FaLocationDot } from "react-icons/fa6";
+
+const dataHome = [
+  {
+    img: `https://studiokhozi.com/wp-content/uploads/2022/02/Pegasus.jpeg`,
+    place: `M3M Alltitude`,
+    sector: `Sector 65, Gurgaon`,
+    price: `On Request`,
+  },
+  {
+    img: `https://api2.smartworksoffice.com/Upload/546fab80-9efa-4fb5-b28a-0d68ad96a15fSmartworks-WTT-Office-faccade.jpg`,
+    place: `White Land Urban Resort`,
+    sector: `Sector 103, Gurgaon`,
+    price: `On Request`,
+  },
+  {
+    img: `https://maxestates.in/wp-content/uploads/2024/01/ms-gallery-big-1.jpg`,
+    place: `M3M Alltitude`,
+    sector: `Sector 65, Gurgaon`,
+    price: `On Request`,
+  },
+  {
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzxcTY-XiEoqdmeob3A0ZylB_2_vl1cO8kZw&s`,
+    place: `M3M Mansion`,
+    sector: `Sector 113, Gurgaon`,
+    price: `On Request`,
+  },
+  {
+    img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzxcTY-XiEoqdmeob3A0ZylB_2_vl1cO8kZw&s`,
+    place: `M3M Alltitude`,
+    sector: `Sector 65, Gurgaon`,
+    price: `On Request`,
+  },
+];
+
+const Card = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "0",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          centerMode: true,
+          centerPadding: "0",
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "0",
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  };
+
+  return (
+   <div>
+    <div>
+      <h2 className=" text-8xl text-center text-[#e49847] py-14">Projects</h2>
+    </div>
+     <div className=" w-full px-10 md:w-2/3 flex flex-col mx-auto">
+      <Slider {...settings}>
+        {dataHome.map((d, index) => (
+          <div key={index} className="relative px-2"> {/* Add padding here */}
+            <div className="card h-[650px] flex flex-col gap-5 rounded-2xl w-[90%] mx-auto bg-white overflow-hidden">
+              <div className="flex">
+                <img
+                  src={d.img}
+                  className="rounded-t-xl h-[400px] w-full"
+                  alt={d.place}
+                />
+              </div>
+              <div className="flex gap-3 flex-col px-5">
+                <p className="text-2xl font-semibold">{d.place}</p>
+                <div className="flex gap-3 items-center">
+                  <FaLocationDot className="text-2xl" />
+                  <p>{d.sector}</p>
+                </div>
+                <p className="flex items-center">
+                  <span className="text-2xl font-extrabold mx-2">&#8377;</span>
+                  <span className="mx-2">{d.price}</span>
+                </p>
+                <button className="mt-2 mx-5 w-[120px] h-[35px] bg-black text-white rounded-xl">
+                  View More
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+   </div>
+  );
+};
+
+export default Card;
